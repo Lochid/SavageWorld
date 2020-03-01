c2e7f994046b7f5bbecdb993a02f635e0e0b85b4
Boris <borsukboris@gmail.com>
2020-03-01 18:13:25 +0300

    antd import fix

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
+import './App.css';
 
 function AppRouter() {
   return (

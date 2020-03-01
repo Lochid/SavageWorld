cbee8496a876d2d86960d29133e49c3bc1b8f28e
ndremov <ndremov@sfxdx.ru>
2020-03-01 13:55:20 +0300

    Add pages

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,36 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, RouteComponentProps } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Dashboard from './pages/dashboard';
 
-type TParams = { id: string | undefined };
-
-function Index() {
-  return <h2>Home</h2>;
-}
-
-function Product({ match }: RouteComponentProps<TParams>) {
-  return <h2>This is a page for product with ID: {match.params.id} </h2>;
-}
 function AppRouter() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/products/1">First Product</Link>
-            </li>
-            <li>
-              <Link to="/products/2">Second Product</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Route path="/" exact component={Index} />
-        <Route path="/products/:id" component={Product} />
-      </div>
+      <Route path="/" exact component={Dashboard} />
     </Router>
   );
 }
